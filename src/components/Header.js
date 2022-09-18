31951ef35961127f206c7ec9e0841c218d2be469
johnmiko <johnmiko4@gmail.com>
2022-09-17 20:00:34 -0400

    allow for onClick as well as hovering

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,44 @@
-import React, { useState } from 'react';
-import Link from './Link';
+import React, { useState } from "react";
+import Link from "./Link";
 
 const Header = () => {
-  const [visible, setVisible] = useState('hidden');
-  const toggleDropdownMenu = () => {
-    if (visible === 'visible'){
-      setVisible('hidden');
-    }        
-    else {
-      setVisible('visible');
-    }          
+  const [visible, setVisible] = useState("hidden");
+  const toggleVisible = () => {
+    if (visible === "visible") {
+      setVisible("hidden");
+    } else {
+      setVisible("visible");
+    }
   };
-
   return (
-    // <div className="ui secondary pointing menu">
     <div className="ui borderless menu">
       <Link href="/" className="item">
-        Resume 
+        Resume
       </Link>
-  <div 
-    className={`ui dropdown item`}
-    onMouseEnter ={() => toggleDropdownMenu()}
-    onMouseLeave ={() => toggleDropdownMenu()}     
-  >
-    Frontend
-    <i class="dropdown icon"></i>
-    <div 
-      className={`menu transition ${visible}`}       
-    >
-      <Link href="/list" className="item">
-        Search
-      </Link>
-      <Link href="/dropdown" className="item">
-        Dropdown
-      </Link>
-      <Link href="/translate" className="item">
-        Translate
-      </Link>
-      <Link href="/css-cheat-sheet" className="item">
-        CSS Cheat Sheet
-      </Link>
+      <div
+        className={`ui dropdown item`}
+        onMouseEnter={() => setVisible("visible")}
+        onMouseLeave={() => setVisible("hidden")}
+      >
+        <div onClick={() => toggleVisible()}>
+          Frontend
+          <i class="dropdown icon"></i>
+        </div>
+        <div className={`menu transition ${visible}`}>
+          <Link href="/list" className="item">
+            Search
+          </Link>
+          <Link href="/dropdown" className="item">
+            Dropdown
+          </Link>
+          <Link href="/translate" className="item">
+            Translate
+          </Link>
+          <Link href="/css-cheat-sheet" className="item">
+            CSS Cheat Sheet
+          </Link>
+        </div>
       </div>
-    </div>
       <Link href="/backend" className="item">
         Backend
       </Link>
